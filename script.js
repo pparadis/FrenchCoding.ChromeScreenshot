2e979aa72dfb6e849442a9549aa7dc6d2d683bef
Pascal Paradis <pascal.paradis@gmail.com>
2012-10-28 14:37:53 -0400

    Renommage de fichier

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.captureVisibleTab(tab.windowId, {"format": "png"}, function(img) {
-    //console.log(img);
     window.open(img);
   });
 });
